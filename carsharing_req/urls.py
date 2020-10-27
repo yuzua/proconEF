d59e20d4521d26aea66ac53168cec78a23b1a209
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-10-28 02:29:19 +0900

    会員登録ページのルートを追記

--- carsharing_req/urls.py
+++ carsharing_req/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import CarsharUser
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hello/', CarsharUser.as_view(), name='hello')
 ]
