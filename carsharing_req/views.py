6c91dcaa0da947d28310b6e7af954dcbfbe4591c
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-10-28 02:28:42 +0900

    会員登録クラスを追加

--- carsharing_req/views.py
+++ carsharing_req/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import CarsharUserCreateForm
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the carsharing_req index.")
+
+
+class CarsharUser(TemplateView):
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'message': 'Not found your data.<br>Please send your profile.',
+            'form': CarsharUserCreateForm(),
+            'link': 'other',
+        }
+    
+    def get(self, request):
+        return render(request, 'carsharing_req/index.html', self.params)
+    
+    def post(self, request):
+        msg = 'Your name is <b>' + request.POST['name'] + \
+            '(' + request.POST['age'] + \
+            ')</b><br>Your mail address is <b>' + request.POST['mail'] + \
+            '</b><br>Thank you send to me!'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'carsharing_req/index.html', self.params)
