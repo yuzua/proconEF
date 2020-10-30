582b1dc9bc0029ab2ecd03e8fe58f01b2a1c486c
student <student@o-hara-kashiwa.local>
2020-10-30 14:20:11 +0900

    103002

--- owners_req/forms.py
+++ owners_req/forms.py
@@ -3,18 +3,40 @@ from.models import CarsharOwnersModel
 
 class CarsharOwnersCreateForm(forms.ModelForm):
     maker_list = [
+        ('レクサス', 'レクサス'),
+        ('トヨタ', 'トヨタ'),
+        ('日産', '日産'),
+        ('ホンダ', 'ホンダ'),
+        ('マツダ', 'マツダ'),
+        ('スバル', 'スバル'),
+        ('スズキ', 'スズキ'),
+        ('三菱', '三菱'),
+        ('ダイハツ', 'ダイハツ'),
+        ('いすゞ', 'いすゞ'),
+        ('光岡自動車', '光岡自動車'),
+        ('トミーカイラ', 'トミーカイラ'),
+        ('ゼロスポーツ', 'ゼロスポーツ'),
+        ('日野自動車', '日野自動車'),
+        ('UDトラックス', '三菱ふそう'),
+        ('GLM', 'GLM'),
+        ('その他外国車', 'その他外国車'),
+    ]
+    car_maker = forms.ChoiceField(label='メーカー', choices=maker_list)
+    type_list = [
         ()
     ]
-    car_maker =
 
-    car_type = 
+    car_type = forms.ChoiceField(label='車種', choices=type_list)
 
     car_possible = forms.IntegerField(label='乗車可能人数',
         widget=forms.TextInput(attrs={'class':'form-control'}))
-    car_number =　forms.IntegerField(label='ナンバープレート(数字のみ入力)',\
+    car_number = forms.IntegerField(label='ナンバープレート(数字のみ入力)',\
         widget=forms.TextInput(attrs={'class':'form-control'}))
-        
-    bank_name =　
+
+    name_list = [
+        ()
+    ]
+    bank_name = forms.ChoiceField(label='銀行選択', choices=name_list)
 
     bank_num = forms.IntegerField(label='支店番号',\
         widget=forms.TextInput(attrs={'class':'form-control'}))
