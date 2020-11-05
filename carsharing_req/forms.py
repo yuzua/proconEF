49a21ad503c1f48b3236c598f825a837af3e4398
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-11-05 15:57:22 +0900

    “郵便番号住所自動入力”

--- carsharing_req/forms.py
+++ carsharing_req/forms.py
@@ -8,15 +8,20 @@ class CarsharUserCreateForm(forms.ModelForm):
         self.fields["gender"].required = False
     class Meta:
         model = CarsharUserModel
-        fields = ['id', 'name', 'gender', 'age', 'birthday']
+        fields = ['id', 'name', 'gender', 'age', 'birthday', 'zip01', 'pref01', 'addr01', 'addr02']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'age': forms.NumberInput(attrs={'class': 'form-control'}),
             'birthday': forms.DateInput(attrs={'class': 'form-control'}),
+            'zip01': forms.TextInput(attrs={'onKeyUp' : "AjaxZip3.zip2addr(this,'','pref01','addr01')"}),
         }
         labels={
            'name': '名前',
            'gender': '性別',
            'age': '年齢',
            'birthday': '生年月日',
+           'zip01': '郵便番号',
+           'pref01': '都道府県名',
+           'addr01': '市町村',
+           'addr02': 'それ以降の住所',
         }
