5e3ed654bb395b8fa2eeb21cd010802236e778e2
student <student@o-hara-kashiwa.local>
2020-11-06 16:12:16 +0900

    2020/11/06/01

--- owners_req/models.py
+++ owners_req/models.py
@@ -30,21 +30,51 @@ class HostUserModel(models.Model):
     def __str__(self):
          return '<カーシェアオーナー:id=' + str(self.id) + ',' + '(' + str(self.bank_name) + ')>'
 
-class CarInfoModel(models.Model):
+# class CarInfoModel(models.Model):
 
-    license_plate = models.CharField(max_length=12)    	
-    ParentCategory = models.CharField(max_length=32)
-    category = models.CharField(max_length=32)
-    model_id = models.CharField(max_length=128)
-    custom = models.CharField(max_length=128)	
-    people = models.IntegerField(default=0)
-    day = models.DateField()
-    tire = models.CharField(max_length=128)
-    used_years = models.IntegerField(default=0)
-    vehicle_inspection_day = models.DateField()
+#     license_plate = models.CharField(max_length=12)    	
+#     ParentCategory = models.CharField(max_length=32)
+#     category = models.CharField(max_length=32)
+#     model_id = models.CharField(max_length=128)
+#     custom = models.CharField(max_length=128)	
+#     people = models.IntegerField(default=0)
+#     day = models.DateField()
+#     tire = models.CharField(max_length=128)
+#     used_years = models.IntegerField(default=0)
+#     vehicle_inspection_day = models.DateField()
+#     def __str__(self):
+#          return '<車情報:id=' + str(self.id) + ',' + '(' + str(self.people) + ')>'
+
+class ParentCategory(models.Model):
+    name = models.CharField('メーカー', max_length=255)
 
     def __str__(self):
-         return '<車情報:id=' + str(self.id) + ',' + '(' + str(self.people) + ')>'
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField('車種', max_length=255)
+    parent = models.ForeignKey(ParentCategory, verbose_name='親カテゴリ', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    
+    user_email = models.EmailField(verbose_name="メールアドレス", max_length=254)
+    category = models.ForeignKey(Category, verbose_name='カテゴリ', default=0, on_delete=models.PROTECT)
+    license_plate = models.CharField(max_length=12,  verbose_name='ナンバープレート')    	
+    #ParentCategory = models.CharField(max_length=32,  verbose_name='メーカー')
+    model_id = models.CharField(max_length=128,  verbose_name='型番')
+    custom = models.CharField(max_length=128,  verbose_name='カスタム')	
+    people = models.IntegerField(default=0,  verbose_name='乗車人数')
+    day = models.DateField( verbose_name='登録日')
+    tire = models.CharField(max_length=128,  verbose_name='タイヤ')
+    used_years = models.IntegerField(default=0,  verbose_name='使用年数')
+    vehicle_inspection_day = models.DateField( verbose_name='次回車検予定日',)
+
+    
     
 
 
