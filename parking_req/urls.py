9aa085c88674ab4fe0e62758ec8180c0b92aa168
student <student@o-hara-kashiwa.local>
2020-11-11 16:04:52 +0900

    post送信によるedit

--- parking_req/urls.py
+++ parking_req/urls.py
@@ -6,7 +6,7 @@ app_name = 'parking_req'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', ParkingHostCreate.as_view(), name='create'),
-    path('edit/<int:num>', views.edit, name='edit'),
+    path('edit', views.edit, name='edit'),
     path('delete/<int:num>', views.delete, name='delete'),
     path('sample', views.sample, name='sample'),
 ]
