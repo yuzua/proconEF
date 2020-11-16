94b58fa88861550304c352873ddafc1e5c264fa0
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-11-16 16:31:09 +0900

    カーシェア予約機能

--- carsharing_booking/views.py
+++ carsharing_booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from carsharing_req .models import CarsharUserModel
+import json
 # Create your views here.
 
 
@@ -25,9 +26,40 @@ def map(request):
     data = CarsharUserModel.objects.get(id=request.session['user_id'])
     print(data.pref01+data.addr01+data.addr02)
     add = data.pref01+data.addr01+data.addr02
+    data = {
+        'markerData': [
+            {
+                'name': 'TAM 東京',
+                'lat': 35.6954806,
+                'lng': 139.76325010000005,
+                'icon': 'tam.png'
+            }, {
+                'name': '小川町駅',
+                'lat': 35.6951212,
+                'lng': 139.76610649999998
+            }, {
+                'name': '淡路町駅',
+                'lat': 35.69496,
+                'lng': 139.76746000000003
+            }, {
+                'name': '御茶ノ水駅',
+                'lat': 35.6993529,
+                'lng': 139.76526949999993
+            }, {
+                'name': '神保町駅',
+                'lat': 35.695932,
+                'lng': 139.75762699999996
+            }, {
+                'name': '新御茶ノ水駅',
+                'lat': 35.696932,
+                'lng': 139.76543200000003
+            }
+        ],
+    }
     params = {
         'name': '自宅',
         'add': add,
+        'data_json': json.dumps(data)
     }
     if (request.method == 'POST'):
         params['add'] = request.POST['add']
