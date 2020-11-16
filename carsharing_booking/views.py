72329aa2a1d7b5ff2462b278053c6797a1b599a2
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-11-16 16:53:52 +0900

    カーシェアリング予約機能

--- carsharing_booking/views.py
+++ carsharing_booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from carsharing_req .models import CarsharUserModel
+from parking_req .models import *
 import json
 # Create your views here.
 
@@ -26,35 +27,40 @@ def map(request):
     data = CarsharUserModel.objects.get(id=request.session['user_id'])
     print(data.pref01+data.addr01+data.addr02)
     add = data.pref01+data.addr01+data.addr02
+    item_all = ParkingUserModel.objects.all()
+    item = item_all.values("id", "user_id", "lat", "lng")
+    item_list = list(item.all())
+    print(item)
     data = {
-        'markerData': [
-            {
-                'name': 'TAM 東京',
-                'lat': 35.6954806,
-                'lng': 139.76325010000005,
-                'icon': 'tam.png'
-            }, {
-                'name': '小川町駅',
-                'lat': 35.6951212,
-                'lng': 139.76610649999998
-            }, {
-                'name': '淡路町駅',
-                'lat': 35.69496,
-                'lng': 139.76746000000003
-            }, {
-                'name': '御茶ノ水駅',
-                'lat': 35.6993529,
-                'lng': 139.76526949999993
-            }, {
-                'name': '神保町駅',
-                'lat': 35.695932,
-                'lng': 139.75762699999996
-            }, {
-                'name': '新御茶ノ水駅',
-                'lat': 35.696932,
-                'lng': 139.76543200000003
-            }
-        ],
+        'markerData': item_list,
+        # 'markerData': [
+        #     {
+        #         'name': 'TAM 東京',
+        #         'lat': 35.6954806,
+        #         'lng': 139.76325010000005,
+        #         'icon': 'tam.png',
+        #     }, {
+        #         'name': '小川町駅',
+        #         'lat': 35.6951212,
+        #         'lng': 139.76610649999998
+        #     }, {
+        #         'name': '淡路町駅',
+        #         'lat': 35.69496,
+        #         'lng': 139.76746000000003
+        #     }, {
+        #         'name': '御茶ノ水駅',
+        #         'lat': 35.6993529,
+        #         'lng': 139.76526949999993
+        #     }, {
+        #         'name': '神保町駅',
+        #         'lat': 35.695932,
+        #         'lng': 139.75762699999996
+        #     }, {
+        #         'name': '新御茶ノ水駅',
+        #         'lat': 35.696932,
+        #         'lng': 139.76543200000003
+        #     }
+        # ],
     }
     params = {
         'name': '自宅',
