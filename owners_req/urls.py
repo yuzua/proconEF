c7de7be89448410c3b7f24d9455f0e4cac45aed2
student <student@o-hara-kashiwa.local>
2020-11-17 15:49:10 +0900

    2020/11/17/01

--- owners_req/urls.py
+++ owners_req/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
-from .views import CreateView, CreateCarView
+from .views import CreateView, CreateCarView, ParkingHostCreate
 
 
 
@@ -17,4 +17,9 @@ urlpatterns = [
     path('deleteCar/<int:num1>', views.deleteCar, name='deleteCar'),
     path('carlist', views.carlist, name='carlist'),
     path('ownerslist', views.ownerslist, name='ownerslist'),
+    path('createParking', ParkingHostCreate.as_view(), name='createParking'),
+    path('editParking', views.editParking, name='editParking'),
+    path('deleteCar/<int:num1>', views.deleteParking, name='deleteParking'),
+    path('parkingplace', views.parkingplace, name='parkingplace'),
+    path('carparkinglist', views.carparkinglist, name='carparkinglist'),
 ]
