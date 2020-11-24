d0afa345981a517cd9a442fdf275103413a0c1c4
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2020-11-24 23:34:10 +0900

    11/24分修正

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 #開発環境では、メールのシステムをスタブ化
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
