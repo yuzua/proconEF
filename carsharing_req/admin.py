58b30c148248e5f6efaba4815f5e60bc954cfcbf
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-01-24 00:33:30 +0900

    お気に入り機能

--- carsharing_req/admin.py
+++ carsharing_req/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CarsharUserModel, UsageModel
+from .models import CarsharUserModel, UsageModel, UserFavoriteCarModel
 
 
 # Register your models here.
 admin.site.register(CarsharUserModel)
 admin.site.register(UsageModel)
+admin.site.register(UserFavoriteCarModel)
